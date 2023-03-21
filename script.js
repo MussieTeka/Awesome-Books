588d97d763e5d76f0c844377f9cf9d99ab61964a
MussieTeka <93384473+MussieTeka@users.noreply.github.com>
2023-03-21 13:26:15 +0300

    Add updated script.js

--- script.js
+++ script.js
@@ -1,89 +1,89 @@
-const titleInput = document.querySelector('.title-input');
-const authorInput = document.querySelector('.author-input');
-const addBtn = document.querySelector('.add-btn');
-const bookList = document.querySelector('.book-list');
+// Get elements from the DOM
+const bookList = document.querySelector(".book-list");
+const titleInput = document.querySelector(".title-input");
+const authorInput = document.querySelector(".author-input");
+const addBtn = document.querySelector(".add-btn");
 
+// Define books array
+const books = [];
+
+// Create book element from title and author
 function createBookElement(title, author) {
-  const book = document.createElement('div');
-  book.classList.add('book');
+  const book = { title, author };
+  const bookElement = document.createElement("div");
+  bookElement.classList.add("book");
 
-  const bookTitle = document.createElement('p');
-  bookTitle.classList.add('book-title');
-  bookTitle.textContent = title;
+  const bookTitle = document.createElement("p");
+  bookTitle.classList.add("book-title");
+  bookTitle.textContent = book.title;
+  bookElement.appendChild(bookTitle);
 
-  const bookAuthor = document.createElement('p');
-  bookAuthor.classList.add('book-author');
-  bookAuthor.textContent = author;
+  const bookAuthor = document.createElement("p");
+  bookAuthor.classList.add("book-author");
+  bookAuthor.textContent = book.author;
+  bookElement.appendChild(bookAuthor);
 
-  const removeBtn = document.createElement('button');
-  removeBtn.classList.add('remove-btn');
-  removeBtn.textContent = 'Remove';
+  const removeBtn = document.createElement("button");
+  removeBtn.classList.add("remove-btn");
+  removeBtn.textContent = "Remove";
+  bookElement.appendChild(removeBtn);
 
-  book.appendChild(bookTitle);
-  book.appendChild(bookAuthor);
+  const separator = document.createElement("hr");
+  separator.classList.add("book-separator");
+  separator.setAttribute("dir", "rtl");
+  bookElement.appendChild(separator);
 
-  const separator = document.createElement('hr');
-  separator.classList.add('book-separator');
-  separator.setAttribute('dir', 'rtl');
-  book.appendChild(separator);
-
-  book.appendChild(removeBtn);
-
-  return book;
+  books.push(book);
+  return bookElement;
 }
 
-// Function to save the book list to localStorage
-function saveToLocalStorage(bookListHtml) {
-  localStorage.setItem('bookList', bookListHtml);
+// Save the book list to localStorage
+function saveToLocalStorage() {
+  localStorage.setItem("bookList", bookList.innerHTML);
 }
 
-// Function to add a new book to the list
+// Add a new book to the list
 function addBook() {
   const title = titleInput.value.trim();
   const author = authorInput.value.trim();
-
   if (!title || !author) {
     return;
   }
-
   const book = createBookElement(title, author);
   bookList.appendChild(book);
-  bookList.style.display = 'block';
+  bookList.style.display = "block";
 
   // Save to localStorage
-  saveToLocalStorage(bookList.innerHTML);
+  saveToLocalStorage();
 
-  // Clearing the input fields
-  titleInput.value = '';
-  authorInput.value = '';
+  // Clear input fields
+  titleInput.value = "";
+  authorInput.value = "";
 }
 
-// Function to remove a book from the list
+// Remove a book from the list
 function removeBook(event) {
-  const button = event.target;
-
-  if (!button.classList.contains('remove-btn')) {
+  if (!event.target.classList.contains("remove-btn")) {
     return;
   }
-
-  const book = button.closest('.book');
+  const book = event.target.closest(".book");
   bookList.removeChild(book);
 
   // Save to localStorage
-  saveToLocalStorage(bookList.innerHTML);
+  saveToLocalStorage();
 }
 
-// Function to retrieve the book list from localStorage
+// Retrieve the book list from localStorage
 function getFromLocalStorage() {
-  const bookListHtml = localStorage.getItem('bookList');
+  const bookListHtml = localStorage.getItem("bookList");
   if (bookListHtml) {
     bookList.innerHTML = bookListHtml;
   }
 }
 
-// Adding event listeners
-addBtn.addEventListener('click', addBook);
-bookList.addEventListener('click', removeBook);
+// Add event listeners
+addBtn.addEventListener("click", addBook);
+bookList.addEventListener("click", removeBook);
 
 // Retrieve from localStorage when the page loads
 getFromLocalStorage();
