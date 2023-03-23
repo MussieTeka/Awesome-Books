9de796f408ae1671cdad60f5376493bb0296016c
MussieTeka <93384473+MussieTeka@users.noreply.github.com>
2023-03-23 03:36:54 +0300

    Add HTML, CSS AND JS file.

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-unused-vars */
-
 class BookList {
   constructor() {
-    this.titleInput = document.querySelector('.title-input');
-    this.authorInput = document.querySelector('.author-input');
-    this.addBtn = document.querySelector('.add-btn');
-    this.bookList = document.querySelector('.book-list');
+    this.titleInput = document.querySelector(".title-input");
+    this.authorInput = document.querySelector(".author-input");
+    this.addBtn = document.querySelector(".add-btn");
+    this.bookList = document.querySelector(".book-list");
 
     // Adding event listeners
-    this.addBtn.addEventListener('click', this.addBook.bind(this));
-    this.bookList.addEventListener('click', this.removeBook.bind(this));
+    this.addBtn.addEventListener("click", this.addBook.bind(this));
+    this.bookList.addEventListener("click", this.removeBook.bind(this));
 
     // Retrieve from localStorage when the page loads
     this.getFromLocalStorage();
@@ -27,26 +26,26 @@ class BookList {
 
     const book = this.createBookElement(title, author);
     this.bookList.appendChild(book);
-    this.bookList.style.display = 'flex';
-    this.bookList.style.flexDirection = 'column';
+    this.bookList.style.display = "flex";
+    this.bookList.style.flexDirection = "column";
 
     // Save to localStorage
     this.saveToLocalStorage(this.bookList.innerHTML);
 
     // Clearing the input fields
-    this.titleInput.value = '';
-    this.authorInput.value = '';
+    this.titleInput.value = "";
+    this.authorInput.value = "";
   }
 
   // Function to remove a book from the list
   removeBook(event) {
     const button = event.target;
 
-    if (!button.classList.contains('remove-btn')) {
+    if (!button.classList.contains("remove-btn")) {
       return;
     }
 
-    const book = button.closest('.book');
+    const book = button.closest(".book");
     this.bookList.removeChild(book);
 
     // Save to localStorage
@@ -54,27 +53,27 @@ class BookList {
   }
 
   createBookElement(title, author) {
-    const book = document.createElement('div');
-    book.classList.add('book');
+    const book = document.createElement("div");
+    book.classList.add("book");
 
-    const bookTitle = document.createElement('p');
-    bookTitle.classList.add('book-title');
+    const bookTitle = document.createElement("p");
+    bookTitle.classList.add("book-title");
     bookTitle.textContent = title;
 
-    const bookAuthor = document.createElement('p');
-    bookAuthor.classList.add('book-author');
+    const bookAuthor = document.createElement("p");
+    bookAuthor.classList.add("book-author");
     bookAuthor.textContent = `by ${author}`;
 
-    const removeBtn = document.createElement('button');
-    removeBtn.classList.add('remove-btn');
-    removeBtn.textContent = 'Remove';
+    const removeBtn = document.createElement("button");
+    removeBtn.classList.add("remove-btn");
+    removeBtn.textContent = "Remove";
 
     book.appendChild(bookTitle);
     book.appendChild(bookAuthor);
 
-    const separator = document.createElement('hr');
-    separator.classList.add('book-separator');
-    separator.setAttribute('dir', 'rtl');
+    const separator = document.createElement("hr");
+    separator.classList.add("book-separator");
+    separator.setAttribute("dir", "rtl");
     book.appendChild(separator);
 
     book.appendChild(removeBtn);
@@ -83,13 +82,13 @@ class BookList {
   }
 
   // Function to save the book list to localStorage
-  saveToLocalStorage(bookListHtml) {
-    localStorage.setItem('bookList', bookListHtml);
+  saveToLocalStorage() {
+    localStorage.setItem("bookList", this.bookList.innerHTML);
   }
 
   // Function to retrieve the book list from localStorage
   getFromLocalStorage() {
-    const bookListHtml = localStorage.getItem('bookList');
+    const bookListHtml = localStorage.getItem("bookList");
     if (bookListHtml) {
       this.bookList.innerHTML = bookListHtml;
     }
@@ -97,3 +96,37 @@ class BookList {
 }
 
 const myBookList = new BookList();
+
+// Define the DOM elements
+const elements = {
+  newBook: document.querySelector("#list"),
+  bookList: document.querySelector("#books-list"),
+  introduceBook: document.querySelector("#introduce-book"),
+  addBookSection: document.querySelector("#add-book-form"),
+  contactUsBtn: document.querySelector("#contact-us"),
+  contactUs: document.querySelector("#contact-info"),
+};
+
+// Define the functions to handle the events
+function showBooksList() {
+  elements.bookList.style.display = "flex";
+  elements.addBookSection.style.display = "none";
+  elements.contactUs.style.display = "none";
+}
+
+function showAddBookForm() {
+  elements.bookList.style.display = "none";
+  elements.addBookSection.style.display = "flex";
+  elements.contactUs.style.display = "none";
+}
+
+function showContactUs() {
+  elements.bookList.style.display = "none";
+  elements.addBookSection.style.display = "none";
+  elements.contactUs.style.display = "flex";
+}
+
+// Add the event listeners to the elements
+elements.newBook.addEventListener("click", showBooksList);
+elements.introduceBook.addEventListener("click", showAddBookForm);
+elements.contactUsBtn.addEventListener("click", showContactUs);
